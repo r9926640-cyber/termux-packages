79b66e0c3952eef3595c754842ee5e0e5111b6a1
r9926640-cyber <r9926640@gmail.com>
2026-09-23 19:08:56 +0530

    Add custom revision patch and bump version

--- packages/zip/build.sh
+++ packages/zip/build.sh
@@ -1,11 +1,11 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/r9926640-cyber/zip30?tab=readme-ov-file
+TERMUX_PKG_HOMEPAGE=https://sourceforge.net/projects/infozip/
 TERMUX_PKG_DESCRIPTION="Tools for working with zip files"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="@RJ"
-TERMUX_PKG_VERSION="3.0.0.1"
-TERMUX_PKG_REVISION=7
-TERMUX_PKG_SRCURL="https://github.com/r9926640-cyber/zip30/archive/refs/heads/main.tar.gz"
-TERMUX_PKG_SHA256=86dd59a49c80ba0007ad417a8c29af57ceeeaa0340b22df6463f6f1ecbde7424
+TERMUX_PKG_VERSION="3.0"
+TERMUX_PKG_REVISION=8
+TERMUX_PKG_SRCURL="https://downloads.sourceforge.net/infozip/zip${TERMUX_PKG_VERSION/.}.tar.gz"
+TERMUX_PKG_SHA256=f0e8bb1f9b7eb0b01285495a2699df3a4b766784c1765a8f1aeedf63c0806369
 TERMUX_PKG_DEPENDS="libandroid-support, libbz2"
 TERMUX_PKG_BUILD_IN_SRC=true
 
